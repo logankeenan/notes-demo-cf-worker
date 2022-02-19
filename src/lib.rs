5da00c76872ee055ad10c598b5d429c15793c929
Logan Keenan <d.logankeenan@gmail.com>
2022-02-19 16:01:00 -0600

    use tide-adapter

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,9 @@
-use std::str::FromStr;
 use notes_demo::AppState;
 use worker::*;
-use tide::http::{Method, Url, Request as TideRequest, Response as TideResponse};
+use tide::http::{Response as TideResponse};
+use tide_adapter::cf_worker;
 
 mod utils;
-
-async fn to_tide_request(mut request: Request) -> TideRequest {
-    let method = Method::from_str(request.method().to_string().as_str()).unwrap();
-    let url = Url::from_str(request.url().unwrap().as_str()).unwrap();
-    let mut tide_request = TideRequest::new(method, url);
-
-    let body_text = request.text().await.unwrap();
-    tide_request.set_body(body_text);
-
-    for (key, value) in request.headers() {
-        tide_request.insert_header(key.as_str(), value.as_str());
-    }
-
-    tide_request
-}
-
-async fn to_worker_response(mut tide_response: TideResponse) -> Response {
-    let bytes = tide_response.body_bytes().await.unwrap();
-    let response = Response::from_bytes(bytes).unwrap();
-    let code: u16 = tide_response.status().to_string().parse().unwrap();
-    let response = response.with_status(code);
-
-    let mut headers = Headers::new();
-
-    tide_response.header_names().for_each(|header_name| {
-        let header_value = tide_response.header(header_name).unwrap();
-        headers.set(header_name.as_str(), header_value.as_str()).unwrap();
-    });
-    let response = response.with_headers(headers);
-
-    response
-}
 
 fn log_request(req: &Request) {
     console_log!(
@@ -56,7 +24,7 @@ pub async fn main(worker_request: Request, env: Env) -> Result<Response> {
     app_state.environment.insert(String::from("API_ORIGIN"), env.var("API_ORIGIN")?.to_string());
     let app = notes_demo::create(app_state);
 
-    let app_response: TideResponse = app.respond(to_tide_request(worker_request).await).await.unwrap();
-    let worker_response = to_worker_response(app_response).await;
+    let app_response: TideResponse = app.respond(cf_worker::to_tide_request(worker_request).await).await.unwrap();
+    let worker_response = cf_worker::to_response(app_response).await;
     Ok(worker_response)
 }
